b13f7acce00f55f556dd728ec9e64d42b70e89e0
Mohammad Al Masalmeh <malmasalmeh.2019@gmail.com>
2026-02-19 18:51:08 -0600

    week 2 finished

--- Week2/week2-demo.js
+++ Week2/week2-demo.js
@@ -5,10 +5,23 @@
 // Write your function here
 function calculateTriangleArea(base, height) {
     // TODO
+    return (1/2) * base * height;
 } 
 
 // Define some variables to test it here
+let base = 5;
+let height = 2;
+
+console.log(calculateTriangleArea(base, height));
 
 // Able to reassign
 
 // Call your function here!
+const _pi = 3.141592653
+
+function calculateCircleArea(radius){
+    return radius * _pi * radius;
+}
+
+let radius = 3;
+console.log(calculateCircleArea(radius));
