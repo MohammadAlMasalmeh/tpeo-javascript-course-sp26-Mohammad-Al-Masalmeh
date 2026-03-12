99d1fe8438b30f3a9b9b47e0cc525641cbcf8be1
Mohammad Al Masalmeh <malmasalmeh.2019@gmail.com>
2026-03-12 18:49:45 -0500

    Week5

--- Week5/week5-demo.js
+++ Week5/week5-demo.js
@@ -3,9 +3,31 @@
 Let's create a constructor for a Student, which takes in their first name, 
 last name, university, major, and graduation year. 
 */ 
+function Student(firstName, lastName, university, major, gradYear){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.university = university;
+    this.major = major;
+    this.gradYear = gradYear;
+}
+
+let Joyce = new Student("Joyce", "Zou", "UT Austin", "CS + Neuro", "2029");
+
 
 
 /*
 Now, create a constructor for a Course, which will take in a student taking the class, 
 the class name, and the class location. 
 */ 
+
+function Course(student, className, classLocation){
+    this.student = student;
+    this.className = className;
+    this.classLocation = classLocation;
+}
+
+let Keondre = new Student("Keondre", "Reese", "UT Austin", "Informatics", "2028");
+let keondreCourse = new Course(Keondre, "TPEO Javascript", "GDC");
+
+console.log(keondreCourse);
+console.log(keondreCourse.student.university);
--- Week5/week5-homework.js
+++ Week5/week5-homework.js
@@ -6,12 +6,31 @@ Car objects. Print out the mpg of each car to the console.
 */
 
 // Your code here! 
+function Car(model, year, color, mpg){
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.mpg = mpg;
+}
 
+let mustang = new Car("Ford Mustang", 2025, "Light-Blue", 16);
+let brokenCamry = new Car("Toyota Camry", 2005, "Grey", 26);
+let magicCarper = new Car("Toyota Rav4", 2018, "Black", 24);
+
+console.log(mustang.color);
 /*
 Question 2: Create a constructor for a Vehicle object. Each Vehicle object should contain only 
 one property: car. Using the 3 Car objects you created previously, create 3 Vehicle objects. 
 Print out the mpg of the car property in each Vehicle object.
 */
+function Vehicle(car, broken){
+    this.car = car;
+    this.broken = broken;
+}
+
+let brokenCamryVehicle = new Vehicle(brokenCamry, true);
+console.log(brokenCamryVehicle);
+console.log(brokenCamryVehicle.car.model);
 
 // Your code here! 
 
