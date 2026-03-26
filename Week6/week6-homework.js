ba862d30e68f9c24042c5b9a41ec45604c549686
Ashwika Katiyar <ashwikakatiyar@gmail.com>
2026-03-26 15:15:36 -0500

    Pushing Week 5 solutions + Week 6 prep

---
+++ Week6/week6-homework.js
@@ -0,0 +1,20 @@
+/*
+Question 1: Write a function, reverse, that accepts one parameter: string. 
+In the function, return the string but reversed (Example: if “hey” is the 
+input, then return “yeh”). Call this function three times each with 
+different inputs and print the results to the terminal.
+*/
+
+/*
+Question 2: Write a function, numVowels, that accepts one parameter: string. 
+In the function, return the number of vowels string has. Call this function 
+three times each with different inputs and print the results to the terminal.
+*/
+
+
+/* 
+Question 3: Write a function, numStringsShorterThanLength that accepts two parameters: 
+strings (an array of strings) and length (a number). In the function, return the number 
+of strings in strings that have a length less than length. Call this function three times 
+each with different inputs and print the results to the terminal.
+*/
